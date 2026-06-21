symfony/ai

[Platform] Add Mock test provider for routing-aware, any-result-type mocks

src/platform/src/Mock/MockResultConverter.php
@@ -1,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Mock;
+
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Result\RawResultInterface;
+use Symfony\AI\Platform\Result\ResultInterface;
+use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\ResultConverterInterface;
+use Symfony\AI\Platform\TokenUsage\TokenUsageExtractorInterface;
+
+/**
+ * Pass-through converter for {@see MockModelClient}: returns the scripted {@see ResultInterface}
+ * verbatim, so any result type is supported without per-type conversion code.
+ *
+ * @author Johannes Wachter <johannes@sulu.io>
+ */
+final class MockResultConverter implements ResultConverterInterface
+{
+    public function supports(Model $model): bool
+    {
+        return true;
+    }
+
+    public function convert(RawResultInterface $result, array $options = []): ResultInterface
+    {
+        $payload = $result->getObject();
+
+        if ($payload instanceof ResultInterface) {
+            return $payload;
+        }
+
+        return new TextResult((string) ($result->getData()['text'] ?? ''));
+    }
+
+    public function getTokenUsageExtractor(): ?TokenUsageExtractorInterface
+    {
+        return null;
+    }
+}

src/platform/tests/Mock/MockModelClientTest.php
@@ -1,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Tests\Mock;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Platform\Exception\InvalidArgumentException;
+use Symfony\AI\Platform\Mock\MockModelClient;
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Result\ObjectResult;
+use Symfony\AI\Platform\Result\TextResult;
+
+final class MockModelClientTest extends TestCase
+{
+    public function testSupportsReturnsTrueForAnyModel()
+    {
+        $client = new MockModelClient('answer');
+
+        $this->assertTrue($client->supports(new Model('any-model')));
+    }
+
+    public function testStringResponseWrapsInTextResult()
+    {
+        $client = new MockModelClient('Hello world');
+
+        $rawResult = $client->request(new Model('any-model'), 'question');
+        $result = $rawResult->getObject();
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame('Hello world', $result->getContent());
+    }
+
+    public function testMapResponseSelectsByModelName()
+    {
+        $client = new MockModelClient([
+            'model-a' => 'answer a',
+            'model-b' => new ObjectResult(['foo' => 'bar']),
+        ]);
+
+        $resultA = $client->request(new Model('model-a'), 'q')->getObject();
+        $resultB = $client->request(new Model('model-b'), 'q')->getObject();
+
+        $this->assertInstanceOf(TextResult::class, $resultA);
+        $this->assertSame('answer a', $resultA->getContent());
+        $this->assertInstanceOf(ObjectResult::class, $resultB);
+        $this->assertSame(['foo' => 'bar'], $resultB->getContent());
+    }
+
+    public function testMapResponseThrowsForUnknownModel()
+    {
+        $client = new MockModelClient(['known' => 'answer']);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('No fake response configured for model "unknown".');
+
+        $client->request(new Model('unknown'), 'q');
+    }
+
+    public function testClosureReceivesModelPayloadAndOptions()
+    {
+        $client = new MockModelClient(static function (Model $model, array|string $payload, array $options): string {
+            return \sprintf('%s|%s|%s', $model->getName(), $payload, $options['stream'] ? 'stream' : 'sync');
+        });
+
+        $result = $client->request(new Model('gpt'), 'hi', ['stream' => true])->getObject();
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame('gpt|hi|stream', $result->getContent());
+    }
+
+    public function testGetCallsRecordsEveryInvocation()
+    {
+        $client = new MockModelClient('answer');
+
+        $client->request(new Model('model-a'), 'first', ['temperature' => 0.5]);
+        $client->request(new Model('model-b'), ['messages' => []]);
+
+        $calls = $client->getCalls();
+
+        $this->assertCount(2, $calls);
+        $this->assertSame('model-a', $calls[0]['model']->getName());
+        $this->assertSame('first', $calls[0]['payload']);
+        $this->assertSame(['temperature' => 0.5], $calls[0]['options']);
+        $this->assertSame('model-b', $calls[1]['model']->getName());
+        $this->assertSame(['messages' => []], $calls[1]['payload']);
+        $this->assertSame([], $calls[1]['options']);
+    }
+}

src/platform/tests/Mock/MockResultConverterTest.php
@@ -1,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Tests\Mock;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Platform\Mock\MockResultConverter;
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Result\InMemoryRawResult;
+use Symfony\AI\Platform\Result\ObjectResult;
+use Symfony\AI\Platform\Result\ResultInterface;
+use Symfony\AI\Platform\Result\Stream\Delta\TextDelta;
+use Symfony\AI\Platform\Result\StreamResult;
+use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\Result\ToolCall;
+use Symfony\AI\Platform\Result\ToolCallResult;
+use Symfony\AI\Platform\Result\VectorResult;
+use Symfony\AI\Platform\Vector\Vector;
+
+final class MockResultConverterTest extends TestCase
+{
+    public function testSupportsReturnsTrueForAnyModel()
+    {
+        $converter = new MockResultConverter();
+
+        $this->assertTrue($converter->supports(new Model('any-model')));
+    }
+
+    public function testGetTokenUsageExtractorReturnsNull()
+    {
+        $converter = new MockResultConverter();
+
+        $this->assertNull($converter->getTokenUsageExtractor());
+    }
+
+    /**
+     * @return iterable<string, array{ResultInterface}>
+     */
+    public static function provideResultTypes(): iterable
+    {
+        yield 'object' => [new ObjectResult(['foo' => 'bar'])];
+        yield 'stream' => [new StreamResult((static function (): \Generator { yield new TextDelta('delta'); })())];
+        yield 'tool-call' => [new ToolCallResult([new ToolCall('id-1', 'my_tool')])];
+        yield 'vector' => [new VectorResult([new Vector([0.1, 0.2, 0.3])])];
+    }
+
+    #[DataProvider('provideResultTypes')]
+    public function testConvertPassesResultThroughUnchanged(ResultInterface $result)
+    {
+        $converter = new MockResultConverter();
+
+        $converted = $converter->convert(new InMemoryRawResult(object: $result));
+
+        $this->assertSame($result, $converted);
+    }
+
+    public function testConvertFallsBackToTextResultFromData()
+    {
+        $converter = new MockResultConverter();
+
+        $converted = $converter->convert(new InMemoryRawResult(['text' => 'plain']));
+
+        $this->assertInstanceOf(TextResult::class, $converted);
+        $this->assertSame('plain', $converted->getContent());
+    }
+
+    public function testConvertFallsBackToEmptyStringWhenNoTextData()
+    {
+        $converter = new MockResultConverter();
+
+        $converted = $converter->convert(new InMemoryRawResult());
+
+        $this->assertInstanceOf(TextResult::class, $converted);
+        $this->assertSame('', $converted->getContent());
+    }
+}

taffy41/ai-platform

src/Test/MockResultConverter.php
@@ -1,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Test;
+
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Result\RawResultInterface;
+use Symfony\AI\Platform\Result\ResultInterface;
+use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\ResultConverterInterface;
+use Symfony\AI\Platform\TokenUsage\TokenUsageExtractorInterface;
+
+/**
+ * Pass-through converter for {@see MockModelClient}: returns the scripted {@see ResultInterface}
+ * verbatim, so any result type is supported without per-type conversion code.
+ *
+ * @author Johannes Wachter <johannes@sulu.io>
+ */
+final class MockResultConverter implements ResultConverterInterface
+{
+    public function supports(Model $model): bool
+    {
+        return true;
+    }
+
+    public function convert(RawResultInterface $result, array $options = []): ResultInterface
+    {
+        $payload = $result->getObject();
+
+        if ($payload instanceof ResultInterface) {
+            return $payload;
+        }
+
+        return new TextResult((string) ($result->getData()['text'] ?? ''));
+    }
+
+    public function getTokenUsageExtractor(): ?TokenUsageExtractorInterface
+    {
+        return null;
+    }
+}

tests/Test/MockModelClientTest.php
@@ -1,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Tests\Test;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Platform\Exception\InvalidArgumentException;
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Result\ObjectResult;
+use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\Test\MockModelClient;
+
+final class MockModelClientTest extends TestCase
+{
+    public function testSupportsReturnsTrueForAnyModel()
+    {
+        $client = new MockModelClient('answer');
+
+        $this->assertTrue($client->supports(new Model('any-model')));
+    }
+
+    public function testStringResponseWrapsInTextResult()
+    {
+        $client = new MockModelClient('Hello world');
+
+        $rawResult = $client->request(new Model('any-model'), 'question');
+        $result = $rawResult->getObject();
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame('Hello world', $result->getContent());
+    }
+
+    public function testMapResponseSelectsByModelName()
+    {
+        $client = new MockModelClient([
+            'model-a' => 'answer a',
+            'model-b' => new ObjectResult(['foo' => 'bar']),
+        ]);
+
+        $resultA = $client->request(new Model('model-a'), 'q')->getObject();
+        $resultB = $client->request(new Model('model-b'), 'q')->getObject();
+
+        $this->assertInstanceOf(TextResult::class, $resultA);
+        $this->assertSame('answer a', $resultA->getContent());
+        $this->assertInstanceOf(ObjectResult::class, $resultB);
+        $this->assertSame(['foo' => 'bar'], $resultB->getContent());
+    }
+
+    public function testMapResponseThrowsForUnknownModel()
+    {
+        $client = new MockModelClient(['known' => 'answer']);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('No scripted response configured for model "unknown".');
+
+        $client->request(new Model('unknown'), 'q');
+    }
+
+    public function testClosureReceivesModelPayloadAndOptions()
+    {
+        $client = new MockModelClient(static function (Model $model, array|string $payload, array $options): string {
+            return \sprintf('%s|%s|%s', $model->getName(), $payload, $options['stream'] ? 'stream' : 'sync');
+        });
+
+        $result = $client->request(new Model('gpt'), 'hi', ['stream' => true])->getObject();
+
+        $this->assertInstanceOf(TextResult::class, $result);
+        $this->assertSame('gpt|hi|stream', $result->getContent());
+    }
+
+    public function testGetCallsRecordsEveryInvocation()
+    {
+        $client = new MockModelClient('answer');
+
+        $client->request(new Model('model-a'), 'first', ['temperature' => 0.5]);
+        $client->request(new Model('model-b'), ['messages' => []]);
+
+        $calls = $client->getCalls();
+
+        $this->assertCount(2, $calls);
+        $this->assertSame('model-a', $calls[0]['model']->getName());
+        $this->assertSame('first', $calls[0]['payload']);
+        $this->assertSame(['temperature' => 0.5], $calls[0]['options']);
+        $this->assertSame('model-b', $calls[1]['model']->getName());
+        $this->assertSame(['messages' => []], $calls[1]['payload']);
+        $this->assertSame([], $calls[1]['options']);
+    }
+}

tests/Test/MockResultConverterTest.php
@@ -1,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Tests\Test;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Result\InMemoryRawResult;
+use Symfony\AI\Platform\Result\ObjectResult;
+use Symfony\AI\Platform\Result\ResultInterface;
+use Symfony\AI\Platform\Result\Stream\Delta\TextDelta;
+use Symfony\AI\Platform\Result\StreamResult;
+use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\Result\ToolCall;
+use Symfony\AI\Platform\Result\ToolCallResult;
+use Symfony\AI\Platform\Result\VectorResult;
+use Symfony\AI\Platform\Test\MockResultConverter;
+use Symfony\AI\Platform\Vector\Vector;
+
+final class MockResultConverterTest extends TestCase
+{
+    public function testSupportsReturnsTrueForAnyModel()
+    {
+        $converter = new MockResultConverter();
+
+        $this->assertTrue($converter->supports(new Model('any-model')));
+    }
+
+    public function testGetTokenUsageExtractorReturnsNull()
+    {
+        $converter = new MockResultConverter();
+
+        $this->assertNull($converter->getTokenUsageExtractor());
+    }
+
+    /**
+     * @return iterable<string, array{ResultInterface}>
+     */
+    public static function provideResultTypes(): iterable
+    {
+        yield 'object' => [new ObjectResult(['foo' => 'bar'])];
+        yield 'stream' => [new StreamResult((static function (): \Generator { yield new TextDelta('delta'); })())];
+        yield 'tool-call' => [new ToolCallResult([new ToolCall('id-1', 'my_tool')])];
+        yield 'vector' => [new VectorResult([new Vector([0.1, 0.2, 0.3])])];
+    }
+
+    #[DataProvider('provideResultTypes')]
+    public function testConvertPassesResultThroughUnchanged(ResultInterface $result)
+    {
+        $converter = new MockResultConverter();
+
+        $converted = $converter->convert(new InMemoryRawResult(object: $result));
+
+        $this->assertSame($result, $converted);
+    }
+
+    public function testConvertFallsBackToTextResultFromData()
+    {
+        $converter = new MockResultConverter();
+
+        $converted = $converter->convert(new InMemoryRawResult(['text' => 'plain']));
+
+        $this->assertInstanceOf(TextResult::class, $converted);
+        $this->assertSame('plain', $converted->getContent());
+    }
+
+    public function testConvertFallsBackToEmptyStringWhenNoTextData()
+    {
+        $converter = new MockResultConverter();
+
+        $converted = $converter->convert(new InMemoryRawResult());
+
+        $this->assertInstanceOf(TextResult::class, $converted);
+        $this->assertSame('', $converted->getContent());
+    }
+}